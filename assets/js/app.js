2730501bb9eab54f8383051e566ea222c903eae1
SAUVAGE <emmanuel.sauvage@live>
2020-06-15 12:43:31 +0200

    add webpack of aminlte

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,10 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
 
+
+//adminLte https://github.com/kevinpapst/AdminLTEBundle/blob/5af0b6cb66f709504b529e96d3d27741336ca220/Resources/docs/extend_webpack_encore.md
+require('../../vendor/kevinpapst/adminlte-bundle/Resources/assets/admin-lte.js');
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 
